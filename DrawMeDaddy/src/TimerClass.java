51284cffce9d185a64f180bc00504f41fd131475
cruztj <ttcruz@up.edu.ph>
2018-05-06 14:14:41 +0800

    Added functionalities to timer class

--- DrawMeDaddy/src/TimerClass.java
+++ DrawMeDaddy/src/TimerClass.java
@@ -6,27 +6,43 @@ public class TimerClass implements Constants{
 	int delay = 1000;
 	int period = 1000;
 
-	public TimerClass(int N){
-		this.seconds = ++N;
+	public TimerClass(int secondsParam){
+		this.seconds = ++secondsParam;
 		Timer time = new Timer();
 		time.scheduleAtFixedRate(
 			new  TimerTask(){
 				
 				public void run(){
+					if(seconds==10){
+						System.out.println("Divide by N");
+						divideTimeByN(2);
+					}
 					if(seconds==1){
 						System.out.println("Time's Up!");
 						time.cancel();
 						time.purge();
 					} else{
-						System.out.println(decreaseN());
+						decreaseSeconds();
+						getCurrentTime();
 					}
 				}
 				
 			}, delay, period);
 	}
 	
-	public int decreaseN(){
-		return --seconds;
+	public void divideTimeByN(int N){
+		if(seconds>0)
+			seconds = seconds/N;
+	}
+
+	private int decreaseSeconds(){
+		--seconds;
+		return seconds;
+	}
+
+	public int getCurrentTime(){
+		System.out.println(seconds);
+		return seconds;
 	}
 
 
